098e8ef03f4ca094110c4f57b07636b3378a0d62
Yohannes Terfa <lightshadow011@gmail.com>
2022-04-10 23:25:46 +0300

    pascals triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+'''
+returns a list of pascals series
+'''
+
+from math import factorial
+
+
+def pascal_triangle(n):
+    '''
+    returns a list of pascal series
+    '''
+    result = []
+    for i in range(n):
+        res = []
+        for j in range(i+1):
+            res.append(factorial(i)//(factorial(j)*factorial(i-j)))
+
+        result.append(res)
+
+    return result
+
+
+def print_triangle(triangle):
+    """
+    Print the triangle
+    """
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
+
+
+if __name__ == "__main__":
+    print_triangle(pascal_triangle(5))
