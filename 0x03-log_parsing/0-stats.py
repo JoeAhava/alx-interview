9d1cc3ceb61171c257416165609cda8a7395be48
Yohannes Terfa <lightshadow011@gmail.com>
2022-06-01 21:38:27 +0300

    log parsing and utf8 validation fix

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,7 +4,7 @@ import sys
 
 
 i = 0
-file_size = 0
+size = 0
 status = {'200': 0, '301': 0, '400': 0, '401': 0,
           '403': 0, '404': 0, '405': 0, '500': 0}
 codes = ['200', '301', '400', '401', '403', '404', '405', '500']
@@ -13,18 +13,18 @@ try:
         i += 1
         sp = line.split(' ')
         if len(sp) > 2:
-            file_size += int(sp[-1])
+            size += int(sp[-1])
             if sp[-2] in status:
                 status[sp[-2]] += 1
         if i % 10 == 0:
-            print("File size: {}".format(file_size))
+            print("File size: {}".format(size))
             for code in codes:
                 if status[code]:
                     print("{}: {}".format(code, status[code]))
 except KeyboardInterrupt:
     pass
 finally:
-    print("File size: {}".format(file_size))
+    print("File size: {}".format(size))
     for code in codes:
         if status[code]:
             print("{}: {}".format(code, status[code]))
