3cfe13690a89736b249e19d52a70e395cb11f16e
Yohannes Terfa <lightshadow011@gmail.com>
2022-06-01 21:36:39 +0300

    log parsing and utf8 validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" checks whether a given data set represents a valid UTF-8 """
+
+
+def EightBits(integer):
+    """
+    converts an integer to its binary representation,
+    """
+    binary = bin(integer)[2:]
+    add = '0' * (8 - len(binary))
+    return add + binary
+
+
+def validUTF8(data):
+    """ checks whether a given data set represents a valid UTF-8 """
+    i = 0
+    while i < len(data):
+        if EightBits(data[i])[0] == '0':
+            i += 1
+        elif (EightBits(data[i])[:3] == '110' and i + 1 < len(data) and
+                EightBits(data[i + 1])[:2] == '10'):
+            i += 2
+        elif (EightBits(data[i])[:4] == '1110' and i + 2 < len(data) and
+                EightBits(data[i + 1])[:2] == '10' and
+                EightBits(data[i + 1])[:2] == '10'):
+            i += 3
+        elif (EightBits(data[i])[:5] == '11110' and i + 3 < len(data) and
+                EightBits(data[i + 1])[:2] == '10' and
+                EightBits(data[i + 2])[:2] == '10' and
+                EightBits(data[i + 3])[:2] == '10'):
+            i += 4
+        else:
+            return False
+    return True
