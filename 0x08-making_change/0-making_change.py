f2b530d2d8faa9c5393407bd81201d1bbba5f741
Yohannes Terfa <lightshadow011@gmail.com>
2022-06-01 19:17:55 +0300

    making Change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+'''
+making change
+'''
+
+
+def makeChange(coins, total):
+    '''
+    returns minimum number of coins
+    to make total
+    '''
+    if total == 0:
+        return 0
+    if not coins or min(coins) > total:
+        return -1
+    num = 0
+    coins.sort(reverse=True)
+    i = 0
+    while i < len(coins):
+        if coins[i] <= total:
+            num += total // coins[i]
+            total = total % coins[i]
+        i += 1
+    if total == 0:
+        return num
+    else:
+        return -1
